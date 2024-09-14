9389c0db4c0544430e80ce9ad87496323dd18423
Fakhruddin Saupe <30628450+Fakhruddin90@users.noreply.github.com>
2024-09-14 21:05:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,3 +74,21 @@ st.write('There are  ' + str(X['T']) + ' thymine (T)')
 st.write('There are  ' + str(X['G']) + ' guanine (G)')
 st.write('There are  ' + str(X['C']) + ' cytosine (C)')
 
+### 3. Display DataFrame
+st.subheader('3. Display DataFrame')
+df = pd.DataFrame.from_dict(X, orient='index')
+df = df.rename({0: 'count'}, axis='columns')
+df.reset_index(inplace=True)
+df = df.rename(columns = {'index':'nucleotide'})
+st.write(df)
+
+### 4. Display Bar Chart using Altair
+st.subheader('4. Display Bar chart')
+p = alt.Chart(df).mark_bar().encode(
+    x='nucleotide',
+    y='count'
+)
+p = p.properties(
+    width=alt.Step(80)  # controls width of bar.
+)
+st.write(p)
