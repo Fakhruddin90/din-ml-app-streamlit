02f5c4f977048a66a418c894a6ea8ee87e88961a
Fakhruddin Saupe <30628450+Fakhruddin90@users.noreply.github.com>
2024-09-14 21:01:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,25 @@
+######################
+# Import libraries
+######################
+
+import pandas as pd
 import streamlit as st
+import altair as alt
+from PIL import Image
 
-st.title('🎈 App Name')
+######################
+# Page Title
+######################
 
-st.write('Hello world!')
+image = Image.open('dna-logo.jpg')
+
+st.image(image, use_column_width=True)
+
+st.write("""
+# DNA Nucleotide Count Web App
+
+This app counts the nucleotide composition of query DNA!
+
+***
+""")
+
